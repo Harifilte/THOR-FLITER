c9cfded6d2f065c678cfa9d71faab5742514c417
Harifilte <114339490+Harifilte@users.noreply.github.com>
2022-10-17 18:53:52 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -379,6 +379,6 @@ def humanbytes(size):
 
 
 async def get_shortlink(link):
-    shortzy = Shortzy("4df33cf5773952f00f9c964d7bb3c5031f5c35a7", "tnlink.in")
+    shortzy = Shortzy("ec72f63f7a24370058610e02813fff3ec317f4c0", "tnlink.in")
     return await shortzy.convert(link)
     
